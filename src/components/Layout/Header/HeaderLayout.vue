e8bc1cf631bf064ccb48e84e2b666c0ed5296551
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-03-31 15:10:00 +0430

    add direct svg

--- src/components/Layout/Header/HeaderLayout.vue
+++ src/components/Layout/Header/HeaderLayout.vue
@@ -20,7 +20,7 @@
               <home-svg></home-svg>
             </router-link>
             <router-link to="/direct/inbox" class="mr-3">
-              <svg class="_8-yf5 router-link-active router-link-exact-active" aria-label="Direct" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><line fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2" x1="22" x2="9.218" y1="3" y2="10.083"></line><polygon fill="none" points="11.698 20.334 22 3.001 2 3.001 9.218 10.084 11.698 20.334" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></polygon></svg>
+              <direct-svg></direct-svg>
             </router-link>
             <li class="mr-3">
               <svg class="_8-yf5" aria-label="New Post" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M2 12v3.45c0 2.849.698 4.005 1.606 4.944.94.909 2.098 1.608 4.946 1.608h6.896c2.848 0 4.006-.7 4.946-1.608C21.302 19.455 22 18.3 22 15.45V8.552c0-2.849-.698-4.006-1.606-4.945C19.454 2.7 18.296 2 15.448 2H8.552c-2.848 0-4.006.699-4.946 1.607C2.698 4.547 2 5.703 2 8.552z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="6.545" x2="17.455" y1="12.001" y2="12.001"></line><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="12.003" x2="12.003" y1="6.545" y2="17.455"></line></svg>
@@ -53,11 +53,13 @@
 import Vue from 'vue'
 import MagnifireSvg from './Components/MagnifireSvg.vue'
 import HomeSvg from './Components/HomeSvg.vue'
+import DirectSvg from './Components/DirectSvg.vue'
 export default Vue.extend({
   name: "HeaderLayout",
   components:{
     MagnifireSvg,
-    HomeSvg
+    HomeSvg,
+    DirectSvg
   }
 });
 </script>
