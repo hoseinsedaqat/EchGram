41c430121ac33edf5cb33c728966f2fcb4c3f83b
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-03-31 15:12:51 +0430

    add file svg

--- src/components/Layout/Header/HeaderLayout.vue
+++ src/components/Layout/Header/HeaderLayout.vue
@@ -23,7 +23,7 @@
               <direct-svg></direct-svg>
             </router-link>
             <li class="mr-3">
-              <svg class="_8-yf5" aria-label="New Post" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M2 12v3.45c0 2.849.698 4.005 1.606 4.944.94.909 2.098 1.608 4.946 1.608h6.896c2.848 0 4.006-.7 4.946-1.608C21.302 19.455 22 18.3 22 15.45V8.552c0-2.849-.698-4.006-1.606-4.945C19.454 2.7 18.296 2 15.448 2H8.552c-2.848 0-4.006.699-4.946 1.607C2.698 4.547 2 5.703 2 8.552z" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></path><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="6.545" x2="17.455" y1="12.001" y2="12.001"></line><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="12.003" x2="12.003" y1="6.545" y2="17.455"></line></svg>
+              <file-svg></file-svg>
             </li>
             <router-link to="/explore" class="mr-3">
               <svg class="_8-yf5" aria-label="Find People" color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><polygon fill="none" points="13.941 13.953 7.581 16.424 10.06 10.056 16.42 7.585 13.941 13.953" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polygon><polygon fill-rule="evenodd" points="10.06 10.056 13.949 13.945 7.581 16.424 10.06 10.056"></polygon><circle cx="12.001" cy="12.005" fill="none" r="10.5" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></circle></svg>
@@ -54,12 +54,14 @@ import Vue from 'vue'
 import MagnifireSvg from './Components/MagnifireSvg.vue'
 import HomeSvg from './Components/HomeSvg.vue'
 import DirectSvg from './Components/DirectSvg.vue'
+import FileSvg from './Components/FileSvg.vue'
 export default Vue.extend({
   name: "HeaderLayout",
   components:{
     MagnifireSvg,
     HomeSvg,
-    DirectSvg
+    DirectSvg,
+    FileSvg
   }
 });
 </script>
