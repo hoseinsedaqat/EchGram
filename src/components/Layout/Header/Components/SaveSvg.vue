3306f6a5e8d0fcc513a3284e605260bf9b79e1c1
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-03-31 20:15:47 +0430

    add setting and save svg

---
+++ src/components/Layout/Header/Components/SaveSvg.vue
@@ -0,0 +1,27 @@
+<template>
+  <svg
+    aria-label="Saved"
+    class="_8-yf5 mr-3"
+    color="#262626"
+    fill="#262626"
+    height="16"
+    role="img"
+    viewBox="0 0 24 24"
+    width="16"
+  >
+    <polygon
+      fill="none"
+      points="20 21 12 13.44 4 21 4 3 20 3 20 21"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+    ></polygon>
+  </svg>
+</template>
+
+<script lang="ts">
+export default {
+  name: "SaveSvg",
+};
+</script>
--- src/components/Layout/Header/HeaderLayout.vue
+++ src/components/Layout/Header/HeaderLayout.vue
@@ -8,6 +8,8 @@ import FileSvg from './Components/FileSvg.vue'
 import ExploreSvg from './Components/ExploreSvg.vue'
 import ActivitySvg from './Components/ActivitySvg.vue'
 import ProfileSvg from './Components/ProfileSvg.vue'
+import SaveSvg from './Components/SaveSvg.vue'
+import SettingSvg from './Components/SettingSvg.vue'
 
 export default Vue.extend({
   name: "HeaderLayout",
@@ -18,7 +20,9 @@ export default Vue.extend({
     FileSvg,
     ExploreSvg,
     ActivitySvg,
-    ProfileSvg
+    ProfileSvg,
+    SaveSvg,
+    SettingSvg
   }
 });
 </script>
@@ -69,13 +73,13 @@ export default Vue.extend({
                     </router-link>
                     <router-link to="/saved" class="dropdown-item">
                      <div class="d-flex align-self-center align-items-center">
-                      <svg aria-label="Saved" class="_8-yf5 mr-3" color="#262626" fill="#262626" height="16" role="img" viewBox="0 0 24 24" width="16"><polygon fill="none" points="20 21 12 13.44 4 21 4 3 20 3 20 21" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polygon></svg>
+                       <save-svg></save-svg>
                       <span>Saved</span>
                      </div>
                     </router-link>
                     <a class="dropdown-item" href="#">
                       <div class="d-flex align-self-center align-items-center">
-                      <svg aria-label="Settings" class="_8-yf5 mr-3" color="#262626" fill="#262626" height="16" role="img" viewBox="0 0 24 24" width="16"><circle cx="12" cy="12" fill="none" r="8.635" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></circle><path d="M14.232 3.656a1.269 1.269 0 01-.796-.66L12.93 2h-1.86l-.505.996a1.269 1.269 0 01-.796.66m-.001 16.688a1.269 1.269 0 01.796.66l.505.996h1.862l.505-.996a1.269 1.269 0 01.796-.66M3.656 9.768a1.269 1.269 0 01-.66.796L2 11.07v1.862l.996.505a1.269 1.269 0 01.66.796m16.688-.001a1.269 1.269 0 01.66-.796L22 12.93v-1.86l-.996-.505a1.269 1.269 0 01-.66-.796M7.678 4.522a1.269 1.269 0 01-1.03.096l-1.06-.348L4.27 5.587l.348 1.062a1.269 1.269 0 01-.096 1.03m11.8 11.799a1.269 1.269 0 011.03-.096l1.06.348 1.318-1.317-.348-1.062a1.269 1.269 0 01.096-1.03m-14.956.001a1.269 1.269 0 01.096 1.03l-.348 1.06 1.317 1.318 1.062-.348a1.269 1.269 0 011.03.096m11.799-11.8a1.269 1.269 0 01-.096-1.03l.348-1.06-1.317-1.318-1.062.348a1.269 1.269 0 01-1.03-.096" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></path></svg>
+                        <setting-svg></setting-svg>
                       <span>Setting</span>
                       </div>
                       </a>
