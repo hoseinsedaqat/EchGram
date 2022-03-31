089c4118fda482852d9f26489862d7e06a1f519a
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-03-31 15:03:54 +0430

    add magnifire svg components

--- src/components/Layout/Header/HeaderLayout.vue
+++ src/components/Layout/Header/HeaderLayout.vue
@@ -9,9 +9,7 @@
           <div>
           <input type="text" name="search" id="search" class="hin" placeholder="Search"/>
           <label for="search" class="hinp">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-              </svg>
+              <magnifire-svg></magnifire-svg>
           </label>
           </div>
         </div>
@@ -35,7 +33,7 @@
             </router-link>
             <li class="mr-3">
               <div class="dropdown">
-                <img type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false" src="../../../assets/Images/Header/HoseinSedaqat.jpg" width="24px" height="24px" style="border-radius:50%;" alt="">
+                <img type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false" src="@/assets/Images/Header/HoseinSedaqat.jpg" width="24px" height="24px" style="border-radius:50%;" alt="">
                   <div class="dropdown-menu dropdown-menu-right mt-3" aria-labelledby="dropdownMenuButton">
                     <a class="dropdown-item" href="#">Action</a>
                     <a class="dropdown-item" href="#">Another action</a>
@@ -51,10 +49,15 @@
   </header>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue from 'vue'
+import MagnifireSvg from './Components/MagnifireSvg.vue'
+export default Vue.extend({
   name: "HeaderLayout",
-};
+  components:{
+    MagnifireSvg
+  }
+});
 </script>
 
 <style scoped>
