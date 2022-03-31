843d2a2cf4a8c13ee7e48830138a1da53138b89e
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-03-31 06:43:12 +0430

    icons it seem good

--- src/components/Layout/Header/HeaderLayout.vue
+++ src/components/Layout/Header/HeaderLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="bg-white py-1 hlb">
     <div class="container">
-      <div class="row mt-1 d-flex flex-row align-item-center justify-content-center">
+      <div class="row mt-1">
         <div class="col-md-4 align-self-center">
           <img src="@/assets/Images/Header/735145cfe0a4.png" alt="EchGram Logo" />
         </div> 
@@ -15,8 +15,9 @@
           </label>
           </div>
         </div>
-        <div class="col-md-4">
-          <ul class="d-flex align-items-end justify-content-around w-100">
+        <div class="col-md-4 hmti">
+          <div>
+            <ul class="d-flex align-items-end justify-content-around">
             <li class="mr-3">
               <svg aria-label="Home" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M22 23h-6.001a1 1 0 01-1-1v-5.455a2.997 2.997 0 10-5.993 0V22a1 1 0 01-1 1H2a1 1 0 01-1-1V11.543a1.002 1.002 0 01.31-.724l10-9.543a1.001 1.001 0 011.38 0l10 9.543a1.002 1.002 0 01.31.724V22a1 1 0 01-1 1z"></path></svg>
             </li>
@@ -36,6 +37,7 @@
               <svg aria-label="Activity Feed" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M16.792 3.904A4.989 4.989 0 0121.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 014.708-5.218 4.21 4.21 0 013.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 013.679-1.938m0-2a6.04 6.04 0 00-4.797 2.127 6.052 6.052 0 00-4.787-2.127A6.985 6.985 0 00.5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 003.518 3.018 2 2 0 002.174 0 45.263 45.263 0 003.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 00-6.708-7.218z"></path></svg>
             </li>
           </ul>
+          </div>
         </div>
       </div>
     </div>
