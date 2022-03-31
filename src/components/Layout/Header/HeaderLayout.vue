acefbacf14f78c9e070c8dc68c43bb5fbe41e773
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-03-31 16:04:17 +0430

    add mt to drop down

--- src/components/Layout/Header/HeaderLayout.vue
+++ src/components/Layout/Header/HeaderLayout.vue
@@ -34,7 +34,7 @@
             <li class="mr-3">
               <div class="dropdown">
                 <img type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false" src="@/assets/Images/Header/HoseinSedaqat.jpg" width="24px" height="24px" style="border-radius:50%;" alt="">
-                  <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+                  <div class="dropdown-menu dropdown-menu-right mt-3" aria-labelledby="dropdownMenuButton">
                     <router-link to="/" class="dropdown-item hlcp">
                       <div class="d-flex align-self-center align-items-center">
                         <svg aria-label="Profile" class="_8-yf5 mr-3" color="#262626" fill="#262626" height="16" role="img" viewBox="0 0 24 24" width="16"><circle cx="12.004" cy="12.004" fill="none" r="10.5" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2"></circle><path d="M18.793 20.014a6.08 6.08 0 00-1.778-2.447 3.991 3.991 0 00-2.386-.791H9.38a3.994 3.994 0 00-2.386.791 6.09 6.09 0 00-1.779 2.447" fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2"></path><circle cx="12.006" cy="9.718" fill="none" r="4.109" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2"></circle></svg>
