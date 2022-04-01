dcd72869c57b980897f32e4b8feca64918d3639b
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-04-01 16:20:15 +0430

    define which section in home layout

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -12,11 +12,20 @@ export default Vue.extend({
       <div class="row">
         <main class="d-flex col-md-12">
           <div class="col-md-8">
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem fugiat libero esse, accusamus quaerat aut, eius similique odit tempora, voluptas vero corrupti. Asperiores alias iste animi sit minus molestias nihil neque modi, autem culpa, qui ipsa dolorum sunt in officiis excepturi nobis? Reiciendis veritatis dolorum illum eveniet, unde et tempora nobis ab. Cumque sit consequuntur tenetur debitis nesciunt, natus laudantium aspernatur explicabo eveniet voluptates quaerat dolores vero nemo placeat. Commodi aut officiis quidem sunt molestiae, provident ad tenetur qui ducimus repellat quae fuga itaque quis harum! Ducimus quo fuga labore officiis enim inventore ipsum vero autem cum culpa, aliquam amet. Doloremque nesciunt sapiente nulla ut perspiciatis libero consequuntur, error id accusamus? Aliquam officia nostrum impedit dolorem omnis expedita tempora iste repellendus vel, consequatur voluptatem aliquid exercitationem unde, laudantium molestias deserunt provident. Corporis non quisquam at ducimus eum doloremque similique sequi voluptatibus vitae unde, aliquam deleniti fuga officia molestias expedita fugit cumque? Obcaecati, ullam, temporibus molestias magnam ad nostrum placeat quasi, adipisci quo perferendis quae rerum aut ipsam distinctio sit laudantium? Quibusdam aliquid eveniet officia deserunt, quo ratione sint dolorum, illum nesciunt ducimus fugit expedita ullam non ipsam vero at velit dicta similique fuga vel quisquam accusamus dignissimos voluptatibus unde? Quos eius itaque optio eveniet, fugiat ab! Modi culpa fugiat doloremque sunt provident minima dolores obcaecati ad dolorem nihil sit ex, placeat voluptatum excepturi non possimus suscipit nobis magni accusamus fuga? Tempora quas consectetur cupiditate laboriosam assumenda minus illo, numquam esse officia natus vitae cumque eaque ex aliquid totam autem ullam! Ipsam praesentium autem amet accusantium et inventore rem ut, deserunt explicabo! Nesciunt placeat atque ab vitae quos quidem obcaecati, eveniet nihil praesentium qui debitis a molestiae enim possimus molestias quaerat perspiciatis suscipit id voluptatibus quasi est voluptate unde. Eaque nobis ea impedit, enim officia fugit atque modi nostrum id! Quas, error? Minima saepe cupiditate ipsa distinctio officiis ea similique, placeat officia alias eos quis accusamus enim asperiores nisi ipsum molestias atque deserunt porro reiciendis hic minus voluptate dolorem impedit. Et deserunt ut quaerat molestiae tempora ea doloremque? Distinctio ad blanditiis error animi nesciunt itaque rerum, similique aliquam mollitia incidunt cupiditate asperiores doloribus impedit commodi, ducimus vitae enim debitis modi id corporis esse adipisci! Quaerat tempora ratione suscipit laudantium minus, magnam sequi unde vitae enim molestias repellendus, consequuntur molestiae ex debitis. Animi accusantium provident unde ducimus, atque tenetur quae fuga eius reprehenderit amet, pariatur dolorum eos optio. Deserunt hic rerum voluptatem!
+            <div class="card mt-4">
+              <div class="card-body">
+                <p>swiper story</p>
+              </div>
+            </div>
+            <div class="card mt-4">
+              <div class="card-body">
+                <p>post</p>
+              </div>
+            </div>
           </div>
           <div class="col-md-4 ho_ps">
             <p class="ho_ps">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptates nesciunt aut laboriosam in sunt, vero repellendus. Dolorum aperiam possimus a aliquam eos eius accusantium ducimus placeat consectetur expedita perspiciatis asperiores saepe enim unde hic, pariatur sapiente officiis ipsa, dolor impedit tempore. Eius iste est quis perferendis id distinctio voluptatum temporibus asperiores, laborum deleniti nemo magnam exercitationem sunt mollitia facere iusto consectetur sequi? Maiores debitis ducimus, quidem incidunt impedit facere ut velit praesentium vero, minus accusamus ab eos maxime ad! Facere placeat repellat nihil obcaecati dolorem perspiciatis nobis rerum numquam nulla doloremque, corporis deleniti neque exercitationem sint iste similique fugit!
+              Write Porfile and Some Suggestion
             </p>
           </div>
         </main>
