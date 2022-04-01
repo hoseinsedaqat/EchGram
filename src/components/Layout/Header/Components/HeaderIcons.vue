021c4f6199b03a522675395aa2fd200aed5aa21c
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-04-01 16:17:01 +0430

    work on home content

--- src/components/Layout/Header/Components/HeaderIcons.vue
+++ src/components/Layout/Header/Components/HeaderIcons.vue
@@ -1,19 +1,19 @@
 <script lang="ts">
 import Vue from "vue";
-import HomeSvg from './HomeSvg.vue';
-import DirectSvg from './DirectSvg.vue';
-import FileSvg from './FileSvg.vue';
-import ExploreSvg from './ExploreSvg.vue';
-import ActivitySvg from './ActivitySvg.vue';
-import ProfileSvg from './ProfileSvg.vue';
-import SaveSvg from './SaveSvg.vue';
-import SettingSvg from './SettingSvg.vue';
-import SwitchSvg from './SwitchSvg.vue';
-import DropdownAngle from './DropdownAngle.vue';
+import HomeSvg from "./HomeSvg.vue";
+import DirectSvg from "./DirectSvg.vue";
+import FileSvg from "./FileSvg.vue";
+import ExploreSvg from "./ExploreSvg.vue";
+import ActivitySvg from "./ActivitySvg.vue";
+import ProfileSvg from "./ProfileSvg.vue";
+import SaveSvg from "./SaveSvg.vue";
+import SettingSvg from "./SettingSvg.vue";
+import SwitchSvg from "./SwitchSvg.vue";
+import DropdownAngle from "./DropdownAngle.vue";
 
 export default Vue.extend({
   name: "HeaderIcons",
-  components:{
+  components: {
     HomeSvg,
     DirectSvg,
     FileSvg,
@@ -23,20 +23,21 @@ export default Vue.extend({
     SaveSvg,
     SettingSvg,
     SwitchSvg,
-    DropdownAngle
-  }
+    DropdownAngle,
+  },
 });
 </script>
 
 <template>
   <ul class="d-flex align-items-end justify-content-around">
+      <!-- Main Icons -->
     <router-link to="/" class="mr-3">
       <home-svg></home-svg>
     </router-link>
     <router-link to="/direct/inbox" class="mr-3">
       <direct-svg></direct-svg>
     </router-link>
-    <router-link to="create/post" class="mr-3">
+    <router-link to="/create/post" class="mr-3">
       <file-svg></file-svg>
     </router-link>
     <router-link to="/explore" class="mr-3">
@@ -46,6 +47,7 @@ export default Vue.extend({
       <activity-svg></activity-svg>
     </router-link>
     <li class="mr-3">
+        <!-- Icons User Profile -->
       <div class="dropdown">
         <img
           type="button"
@@ -56,14 +58,14 @@ export default Vue.extend({
           width="24px"
           height="24px"
           style="border-radius: 50%"
-          alt=""
+          alt="Hosein Sedaqat"
         />
         <div
           class="dropdown-menu dropdown-menu-right mt-3"
           aria-labelledby="dropdownMenuButton"
         >
           <dropdown-angle></dropdown-angle>
-          <router-link to="/" class="dropdown-item hlcp">
+          <router-link to="/" class="dropdown-item h_lcp">
             <div class="d-flex align-self-center align-items-center">
               <profile-svg></profile-svg>
               <span class="align-self-center">Profile</span>
--- src/components/Layout/Header/HeaderLayout.vue
+++ src/components/Layout/Header/HeaderLayout.vue
@@ -1,39 +1,45 @@
 <script lang="ts">
-
-import Vue from 'vue';
-import HeaderIcons from './Components/HeaderIcons.vue';
-import MagnifireSvg from './Components/MagnifireSvg.vue';
+import Vue from "vue";
+import HeaderIcons from "./Components/HeaderIcons.vue";
+import MagnifireSvg from "./Components/MagnifireSvg.vue";
 
 export default Vue.extend({
   name: "HeaderLayout",
-  components:{
+  components: {
     MagnifireSvg,
-    HeaderIcons
-  }
+    HeaderIcons,
+  },
 });
 </script>
-    HeaderIcons
-
+HeaderIcons
 
 <template>
-  <header class="bg-white py-1 hlb" id="hbh">
+  <header class="bg-white py-1 h_lb" id="h_bh">
     <div class="container">
       <div class="row mt-1">
         <!-- Logo -->
         <div class="col-md-4 align-self-center">
-          <img src="@/assets/Images/Header/735145cfe0a4.png" alt="EchGram Logo" />
-        </div> 
+          <router-link to="/">
+            <img src="@/assets/Images/Header/735145cfe0a4.png" alt="EchGram Logo" />
+          </router-link>
+        </div>
         <!-- Header Input -->
         <div class="col-md-4 align-self-center">
           <div>
-          <input type="text" name="search" id="search" class="hin" placeholder="Search"/>
-          <label for="search" class="hinp">
+            <input
+              type="text"
+              name="search"
+              id="search"
+              class="h_in"
+              placeholder="Search"
+            />
+            <label for="search" class="h_inp">
               <magnifire-svg></magnifire-svg>
-          </label>
+            </label>
           </div>
         </div>
         <!-- Icons Route -->
-        <div class="col-md-4 hmti">
+        <div class="col-md-4 h_mti">
           <div>
             <header-icons></header-icons>
           </div>
