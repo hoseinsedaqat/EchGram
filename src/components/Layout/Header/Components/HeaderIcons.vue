bb29abfde11e1acc447dd90dfa855e43f80a6b24
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-04-01 15:49:13 +0430

    move icons part of header to new components

---
+++ src/components/Layout/Header/Components/HeaderIcons.vue
@@ -0,0 +1,95 @@
+<script lang="ts">
+import Vue from "vue";
+import HomeSvg from './HomeSvg.vue';
+import DirectSvg from './DirectSvg.vue';
+import FileSvg from './FileSvg.vue';
+import ExploreSvg from './ExploreSvg.vue';
+import ActivitySvg from './ActivitySvg.vue';
+import ProfileSvg from './ProfileSvg.vue';
+import SaveSvg from './SaveSvg.vue';
+import SettingSvg from './SettingSvg.vue';
+import SwitchSvg from './SwitchSvg.vue';
+import DropdownAngle from './DropdownAngle.vue';
+
+export default Vue.extend({
+  name: "HeaderIcons",
+  components:{
+    HomeSvg,
+    DirectSvg,
+    FileSvg,
+    ExploreSvg,
+    ActivitySvg,
+    ProfileSvg,
+    SaveSvg,
+    SettingSvg,
+    SwitchSvg,
+    DropdownAngle
+  }
+});
+</script>
+
+<template>
+  <ul class="d-flex align-items-end justify-content-around">
+    <router-link to="/" class="mr-3">
+      <home-svg></home-svg>
+    </router-link>
+    <router-link to="/direct/inbox" class="mr-3">
+      <direct-svg></direct-svg>
+    </router-link>
+    <router-link to="create/post" class="mr-3">
+      <file-svg></file-svg>
+    </router-link>
+    <router-link to="/explore" class="mr-3">
+      <explore-svg></explore-svg>
+    </router-link>
+    <router-link to="/accounts/activity" class="mr-3">
+      <activity-svg></activity-svg>
+    </router-link>
+    <li class="mr-3">
+      <div class="dropdown">
+        <img
+          type="button"
+          id="dropdownMenuButton"
+          data-toggle="dropdown"
+          aria-expanded="false"
+          src="@/assets/Images/Header/HoseinSedaqat.jpg"
+          width="24px"
+          height="24px"
+          style="border-radius: 50%"
+          alt=""
+        />
+        <div
+          class="dropdown-menu dropdown-menu-right mt-3"
+          aria-labelledby="dropdownMenuButton"
+        >
+          <dropdown-angle></dropdown-angle>
+          <router-link to="/" class="dropdown-item hlcp">
+            <div class="d-flex align-self-center align-items-center">
+              <profile-svg></profile-svg>
+              <span class="align-self-center">Profile</span>
+            </div>
+          </router-link>
+          <router-link to="/saved" class="dropdown-item">
+            <div class="d-flex align-self-center align-items-center">
+              <save-svg></save-svg>
+              <span>Saved</span>
+            </div>
+          </router-link>
+          <router-link to="/" class="dropdown-item">
+            <div class="d-flex align-self-center align-items-center">
+              <setting-svg></setting-svg>
+              <span>Setting</span>
+            </div>
+          </router-link>
+          <router-link to="/" class="dropdown-item" style="border-bottom: 2px solid #eee">
+            <div class="d-flex align-self-center align-items-center">
+              <switch-svg></switch-svg>
+              <span>Switch Accounts</span>
+            </div>
+          </router-link>
+          <router-link to="/" class="dropdown-item">Log Out</router-link>
+        </div>
+      </div>
+    </li>
+  </ul>
+</template>
--- src/components/Layout/Header/HeaderLayout.vue
+++ src/components/Layout/Header/HeaderLayout.vue
@@ -1,35 +1,18 @@
 <script lang="ts">
 
 import Vue from 'vue';
+import HeaderIcons from './Components/HeaderIcons.vue';
 import MagnifireSvg from './Components/MagnifireSvg.vue';
-import HomeSvg from './Components/HomeSvg.vue';
-import DirectSvg from './Components/DirectSvg.vue';
-import FileSvg from './Components/FileSvg.vue';
-import ExploreSvg from './Components/ExploreSvg.vue';
-import ActivitySvg from './Components/ActivitySvg.vue';
-import ProfileSvg from './Components/ProfileSvg.vue';
-import SaveSvg from './Components/SaveSvg.vue';
-import SettingSvg from './Components/SettingSvg.vue';
-import SwitchSvg from './Components/SwitchSvg.vue';
-import DropdownAngle from './Components/DropdownAngle.vue';
 
 export default Vue.extend({
   name: "HeaderLayout",
   components:{
     MagnifireSvg,
-    HomeSvg,
-    DirectSvg,
-    FileSvg,
-    ExploreSvg,
-    ActivitySvg,
-    ProfileSvg,
-    SaveSvg,
-    SettingSvg,
-    SwitchSvg,
-    DropdownAngle
+    HeaderIcons
   }
 });
 </script>
+    HeaderIcons
 
 
 <template>
@@ -52,56 +35,7 @@ export default Vue.extend({
         <!-- Icons Route -->
         <div class="col-md-4 hmti">
           <div>
-            <ul class="d-flex align-items-end justify-content-around">
-            <router-link to="/" class="mr-3">
-              <home-svg></home-svg>
-            </router-link>
-            <router-link to="/direct/inbox" class="mr-3">
-              <direct-svg></direct-svg>
-            </router-link>
-            <li class="mr-3">
-              <file-svg></file-svg>
-            </li>
-            <router-link to="/explore" class="mr-3">
-              <explore-svg></explore-svg>
-            </router-link>
-            <router-link to="/accounts/activity" class="mr-3">
-             <activity-svg></activity-svg>
-            </router-link>
-            <li class="mr-3">
-              <div class="dropdown">
-                <img type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false" src="@/assets/Images/Header/HoseinSedaqat.jpg" width="24px" height="24px" style="border-radius:50%;" alt="">
-                  <div class="dropdown-menu dropdown-menu-right mt-3" aria-labelledby="dropdownMenuButton">
-                    <dropdown-angle></dropdown-angle>
-                    <router-link to="/" class="dropdown-item hlcp">
-                      <div class="d-flex align-self-center align-items-center">
-                        <profile-svg></profile-svg>
-                      <span class="align-self-center">Profile</span>
-                      </div>
-                    </router-link>
-                    <router-link to="/saved" class="dropdown-item">
-                     <div class="d-flex align-self-center align-items-center">
-                       <save-svg></save-svg>
-                      <span>Saved</span>
-                     </div>
-                    </router-link>
-                    <a class="dropdown-item" href="#">
-                      <div class="d-flex align-self-center align-items-center">
-                        <setting-svg></setting-svg>
-                      <span>Setting</span>
-                      </div>
-                      </a>
-                    <a class="dropdown-item" href="#" style="border-bottom:2px solid #eee;">
-                      <div class="d-flex align-self-center align-items-center">
-                        <switch-svg></switch-svg>
-                      <span>Switch Accounts</span>
-                      </div>
-                      </a>
-                    <a class="dropdown-item" href="#">Log Out</a>
-                  </div>
-              </div>
-            </li>
-          </ul>
+            <header-icons></header-icons>
           </div>
         </div>
       </div>
