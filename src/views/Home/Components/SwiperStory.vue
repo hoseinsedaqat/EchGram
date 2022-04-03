b3c6893edc46f8100c990a345259ef5992c998fb
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-04-03 20:31:26 +0430

    move each part of home layout to specif components

---
+++ src/views/Home/Components/SwiperStory.vue
@@ -0,0 +1,124 @@
+<script lang="ts">
+import Vue from 'vue'
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
+
+export default Vue.extend({
+    name:"SwiperStory",
+     data() {
+        return {
+        swiperOption: {
+            slidesPerView: 7,
+            spaceBetween: 2,
+            loop: false,
+            loopFillGroupWithBlank: true,
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        },
+    };
+  },
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+})
+</script>
+
+
+
+<template>
+  <div class="card mt-4">
+              <!-- Swiper Story -->
+              <div>
+                <swiper class="swiper" :options="swiperOption">
+                  <swiper-slide>
+                    <router-link to="/test">
+                    <img class="ho_cp ho_bcs" src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="" width="65px" style="border-radius:50%;">
+                    </router-link>
+                  </swiper-slide>
+                  <swiper-slide>
+                    <img class="ho_cp ho_bcs" src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="" width="65px" style="border-radius:50%;">
+                  </swiper-slide>
+                  <swiper-slide>
+                    <img class="ho_cp ho_bcs" src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="" width="65px" style="border-radius:50%;">
+                  </swiper-slide>
+                  <swiper-slide>
+                    <img class="ho_cp ho_bcs" src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="" width="65px" style="border-radius:50%;">
+                  </swiper-slide>
+                  <swiper-slide>
+                    <img class="ho_cp ho_bcs" src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="" width="65px" style="border-radius:50%;">
+                  </swiper-slide>
+                  <swiper-slide>
+                    <img class="ho_cp ho_bcs" src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="" width="65px" style="border-radius:50%;">
+                  </swiper-slide>
+                  <swiper-slide>
+                    <img class="ho_cp ho_bcs" src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="" width="65px" style="border-radius:50%;">
+                  </swiper-slide>
+                  <swiper-slide>
+                    <img class="ho_cp ho_bcs" src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="" width="65px" style="border-radius:50%;">
+                  </swiper-slide>
+                  <swiper-slide>
+                    <img class="ho_cp ho_bcs" src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="" width="65px" style="border-radius:50%;">
+                  </swiper-slide>
+                  <div class="swiper-button-prev" slot="button-prev"></div>
+                  <div class="swiper-button-next" slot="button-next"></div>
+                </swiper>
+              </div>
+            </div>
+</template>
+
+
+<style scoped>
+.swiper {
+  height: 100px;
+  width: 100%;
+}
+.swiper-wrapper {
+  width: 100% !important;
+  height: 100%;
+}
+.swiper-slide {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-weight: bold;
+  font-size: 14px;
+  background-color: #fff;
+  border-radius: 10px;
+}
+.swiper-button-next {
+  left: 1;
+  top: 35px;
+  padding: 2.5rem 0.2rem;
+  color: #333;
+  transition: all 0.4s ease;
+}
+.swiper-button-next:hover {
+  transform: translate(-2px);
+}
+.swiper-button-next::after {
+  font-size: 10px;
+  background-color: #fff;
+  padding: 0.4rem;
+  border-radius: 50%;
+}
+.swiper-button-prev {
+  right: 1;
+  top: 35px;
+  padding: 2.5rem 0.2rem;
+  color: #333;
+  transition: all 0.4s ease;
+}
+.swiper-button-prev:hover {
+  transform: translate(2px);
+}
+.swiper-button-prev::after {
+  font-size: 10px;
+  background-color: #fff;
+  padding: 0.4rem;
+  border-radius: 50%;
+}
+</style>
--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,28 +1,9 @@
 <script lang="ts">
 import Vue from "vue";
-import { Swiper, SwiperSlide } from "vue-awesome-swiper";
-import "swiper/css/swiper.css";
-
+import SwiperStory from "./Components/SwiperStory.vue";
 export default Vue.extend({
+  components: { SwiperStory },
   name: "HomeView",
-  data() {
-    return {
-      swiperOption: {
-        slidesPerView: 7,
-        spaceBetween: 2,
-        loop: false,
-        loopFillGroupWithBlank: true,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      },
-    };
-  },
-  components: {
-    Swiper,
-    SwiperSlide,
-  },
   methods: {
     test(){
       alert('New Comment!😃')
@@ -37,44 +18,7 @@ export default Vue.extend({
       <div class="row">
         <main class="d-flex col-md-12">
           <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 mt-3">
-            <div class="card mt-4">
-              <!-- Swiper Story -->
-              <div>
-                <swiper class="swiper" :options="swiperOption">
-                  <swiper-slide>
-                    <router-link to="/test">
-                    <img class="ho_cp ho_bcs" src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="" width="65px" style="border-radius:50%;">
-                    </router-link>
-                  </swiper-slide>
-                  <swiper-slide>
-                    <img class="ho_cp ho_bcs" src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="" width="65px" style="border-radius:50%;">
-                  </swiper-slide>
-                  <swiper-slide>
-                    <img class="ho_cp ho_bcs" src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="" width="65px" style="border-radius:50%;">
-                  </swiper-slide>
-                  <swiper-slide>
-                    <img class="ho_cp ho_bcs" src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="" width="65px" style="border-radius:50%;">
-                  </swiper-slide>
-                  <swiper-slide>
-                    <img class="ho_cp ho_bcs" src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="" width="65px" style="border-radius:50%;">
-                  </swiper-slide>
-                  <swiper-slide>
-                    <img class="ho_cp ho_bcs" src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="" width="65px" style="border-radius:50%;">
-                  </swiper-slide>
-                  <swiper-slide>
-                    <img class="ho_cp ho_bcs" src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="" width="65px" style="border-radius:50%;">
-                  </swiper-slide>
-                  <swiper-slide>
-                    <img class="ho_cp ho_bcs" src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="" width="65px" style="border-radius:50%;">
-                  </swiper-slide>
-                  <swiper-slide>
-                    <img class="ho_cp ho_bcs" src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="" width="65px" style="border-radius:50%;">
-                  </swiper-slide>
-                  <div class="swiper-button-prev" slot="button-prev"></div>
-                  <div class="swiper-button-next" slot="button-next"></div>
-                </swiper>
-              </div>
-            </div>
+            <swiper-story></swiper-story>
             <!-- Post Section -->
             <div class="card my-4">
               <div class="py-2">
@@ -227,55 +171,4 @@ export default Vue.extend({
   </section>
 </template>
 
-<style scoped>
-.swiper {
-  height: 100px;
-  width: 100%;
-}
-.swiper-wrapper {
-  width: 100% !important;
-  height: 100%;
-}
-.swiper-slide {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  font-weight: bold;
-  font-size: 14px;
-  background-color: #fff;
-  border-radius: 10px;
-}
-.swiper-button-next {
-  left: 1;
-  top: 35px;
-  padding: 2.5rem 0.2rem;
-  color: #333;
-  transition: all 0.4s ease;
-}
-.swiper-button-next:hover {
-  transform: translate(-2px);
-}
-.swiper-button-next::after {
-  font-size: 10px;
-  background-color: #fff;
-  padding: 0.4rem;
-  border-radius: 50%;
-}
-.swiper-button-prev {
-  right: 1;
-  top: 35px;
-  padding: 2.5rem 0.2rem;
-  color: #333;
-  transition: all 0.4s ease;
-}
-.swiper-button-prev:hover {
-  transform: translate(2px);
-}
-.swiper-button-prev::after {
-  font-size: 10px;
-  background-color: #fff;
-  padding: 0.4rem;
-  border-radius: 50%;
-}
-</style>
+
