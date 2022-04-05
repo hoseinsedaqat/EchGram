8d4bdb2cf7efa54e724b9ba20a6538784326c467
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-04-05 22:44:16 +0430

    add header of direct with message

--- src/views/Direct/DirectView.vue
+++ src/views/Direct/DirectView.vue
@@ -1,3 +1,23 @@
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: "DirectView",
+  data(){
+    return {
+      noneDirect: true,
+      directOne: false
+    }
+  },
+  methods: {
+    directUserOne(){
+      this.noneDirect = false;
+      this.directOne = true
+    }
+  }
+});
+</script>
+
 <template>
   <div class="container">
     <div class="row my-3 bg-white no-gutters">
@@ -14,7 +34,7 @@
             </div>
           </div>
           <div class="d_bp d_of">
-            <div class="d-flex py-3 d_ph">
+            <div class="d-flex py-3 d_ph" @click="directUserOne">
                 <span class="mx-2">
                   <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="DirectHosein" width="50px" style="border-radius:50%;">
                 </span>
@@ -89,8 +109,9 @@
           </div>
         </div>
         <div class="col-md-8">
-          <div class="d_bp d-flex w-100 h-100 align-items-center justify-content-center text-center">
-            <div class="d-flex flex-column align-items-center justify-content-center">
+          <div class="d_bp w-100 h-100 text-center">
+            <!-- When We are have no Direct Select -->
+            <div class="d-flex flex-column align-items-center justify-content-center w-100 h-100" v-if="noneDirect">
             <h3 class="d_echl">Echgram</h3>
             <p class="h5">Your Messages</p>
             <span class="text-muted">
@@ -98,14 +119,21 @@
             </span>
             <button class="btn btn-primary w-50 my-3">Send Message</button>
             </div>
+            <!-- When We are have Direct -->
+            <div v-if="directOne" class="w-100 d_bp">
+              <!-- Direct Msg Header -->
+              <div class="d-flex align-items-center justify-content-between d_mfhm mx-5">
+                <span>
+                  <img class="mr-3" src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinMessage"  width="30px" style="border-radius:50%;">
+                  <small class="font-weight-bold">Hosein</small>
+                </span>
+                <span>
+                  <svg aria-label="View Thread Details" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><circle cx="12.001" cy="12.005" fill="none" r="10.5" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></circle><circle cx="11.819" cy="7.709" r="1.25"></circle><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="10.569" x2="13.432" y1="16.777" y2="16.777"></line><polyline fill="none" points="10.569 11.05 12 11.05 12 16.777" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polyline></svg>
+                </span>
+              </div>
+            </div>
           </div>
         </div>
     </div>
   </div>
 </template>
-
-<script>
-export default {
-  name: "DirectView",
-};
-</script>
