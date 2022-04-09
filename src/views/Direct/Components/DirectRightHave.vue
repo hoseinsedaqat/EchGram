381ba297a97420b10a513028a2502d23b503ea31
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-04-09 08:52:35 +0430

    add cp to header direct have

--- src/views/Direct/Components/DirectRightHave.vue
+++ src/views/Direct/Components/DirectRightHave.vue
@@ -19,7 +19,7 @@ export default Vue.extend({
                   <img class="mr-3" src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinMessage"  width="30px" style="border-radius:50%;">
                   <small class="font-weight-bold">Hosein</small>
                 </span>
-                <span>
+                <span class="h_lcp">
                   <svg aria-label="View Thread Details" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><circle cx="12.001" cy="12.005" fill="none" r="10.5" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></circle><circle cx="11.819" cy="7.709" r="1.25"></circle><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="10.569" x2="13.432" y1="16.777" y2="16.777"></line><polyline fill="none" points="10.569 11.05 12 11.05 12 16.777" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polyline></svg>
                 </span>
               </div>
