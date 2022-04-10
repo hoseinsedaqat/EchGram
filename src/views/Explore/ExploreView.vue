ddbbd53b483fb753d6ea426c5bcaff8abcf286ee
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-04-10 21:44:40 +0430

    add fake data to fix explore pages

--- src/views/Explore/ExploreView.vue
+++ src/views/Explore/ExploreView.vue
@@ -18,6 +18,15 @@ export default Vue.extend({
                  <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="ex_iw img-fluid">
              </div>
          </div>
+         <div class="row my-4">
+             <div class="col-md-4">
+                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="img-fluid w-100">
+                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="mt-2 img-fluid w-100">
+             </div>
+             <div class="col-md-8">
+                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="ex_iw img-fluid">
+             </div>
+         </div>
      </div>
  </section>
 </template>
