2f0449ce35f1d0ae9059fac1b83f12d128abdc6a
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-04-10 21:55:37 +0430

    move explore layout to other components

---
+++ src/views/Explore/Components/ExploreLayout.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    name:"ExploreLayout"
+})
+</script>
+
+<template>
+  <div class="container">
+         <div class="row my-4">
+             <div class="col-md-4">
+                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="img-fluid w-100">
+                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="mt-2 img-fluid w-100">
+             </div>
+             <div class="col-md-8">
+                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="ex_iw img-fluid">
+             </div>
+         </div>
+         <div class="row my-4">
+             <div class="col-md-4">
+                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="img-fluid w-100">
+                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="mt-2 img-fluid w-100">
+             </div>
+             <div class="col-md-8">
+                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="ex_iw img-fluid">
+             </div>
+         </div>
+    </div>
+</template>
--- src/views/Explore/ExploreView.vue
+++ src/views/Explore/ExploreView.vue
@@ -1,32 +1,15 @@
 <script lang="ts">
 import Vue from 'vue'
+import ExploreLayout from './Components/ExploreLayout.vue'
 
 export default Vue.extend({
+  components: { ExploreLayout },
     name:"ExploreView"
 })
 </script>
 
 <template>
  <section id="exploreView">
-     <div class="container">
-         <div class="row my-4">
-             <div class="col-md-4">
-                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="img-fluid w-100">
-                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="mt-2 img-fluid w-100">
-             </div>
-             <div class="col-md-8">
-                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="ex_iw img-fluid">
-             </div>
-         </div>
-         <div class="row my-4">
-             <div class="col-md-4">
-                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="img-fluid w-100">
-                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="mt-2 img-fluid w-100">
-             </div>
-             <div class="col-md-8">
-                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="ex_iw img-fluid">
-             </div>
-         </div>
-     </div>
+    <explore-layout></explore-layout>
  </section>
 </template>
