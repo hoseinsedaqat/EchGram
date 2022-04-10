7cd2eff5ec037259fec76865455c312c43cf38eb
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-04-10 21:33:22 +0430

    work on explore pages

--- src/views/Explore/ExploreView.vue
+++ src/views/Explore/ExploreView.vue
@@ -9,9 +9,13 @@ export default Vue.extend({
 <template>
  <section id="exploreView">
      <div class="container">
-         <div class="row">
-             <div class="col-md-12 my-4">
-                 <h3>Explore View</h3>
+         <div class="row my-4">
+             <div class="col-md-4">
+                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="img-fluid w-100">
+                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="mt-2 img-fluid w-100">
+             </div>
+             <div class="col-md-8">
+                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="ex_iw img-fluid">
              </div>
          </div>
      </div>
