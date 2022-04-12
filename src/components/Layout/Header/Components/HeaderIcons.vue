021b7428cead6e242cec82507032c19156e7a1cd
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-04-12 20:54:31 +0430

    work on header show which route

--- src/components/Layout/Header/Components/HeaderIcons.vue
+++ src/components/Layout/Header/Components/HeaderIcons.vue
@@ -29,6 +29,9 @@ export default Vue.extend({
     return {
       homeNav: true,
       directNav: true,
+      postNav:true,
+      exploreNav:true,
+      activityNav:true
     };
   },
   // now i think can change color of
@@ -37,13 +40,40 @@ export default Vue.extend({
     $route(to, from) {
       switch (to.path) {
         case "/":
-          this.homeNav = false;
-          this.directNav = true;
+          this.homeNav = false
+          this.directNav = true
+          this.postNav = true
+          this.exploreNav = true
+          this.activityNav = true
           break;
         case "/direct/inbox":
-          this.homeNav = true;
-          this.directNav = false;
+          this.homeNav = true
+          this.directNav = false
+          this.postNav = true
+          this.exploreNav = true
+          this.activityNav = true
           break;
+        case "/create/post":
+          this.homeNav = true
+          this.directNav = true
+          this.postNav = false
+          this.exploreNav = true
+          this.activityNav = true
+          break
+        case "/explore":
+          this.homeNav = true
+          this.directNav = true
+          this.postNav = true
+          this.exploreNav = false
+          this.activityNav = true
+          break
+        case "/accounts/activity":
+          this.homeNav = true
+          this.directNav = true
+          this.postNav = true
+          this.exploreNav = true
+          this.activityNav = false
+          break
       }
     },
   },
@@ -94,13 +124,16 @@ export default Vue.extend({
       </svg>
     </router-link>
     <router-link to="/create/post" class="mr-3">
-      <file-svg></file-svg>
+      <file-svg v-if="postNav"></file-svg>
+      <svg v-if="!postNav" aria-label="New Post" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M12.003 5.545l-.117.006-.112.02a1 1 0 00-.764.857l-.007.117V11H6.544l-.116.007a1 1 0 00-.877.876L5.545 12l.007.117a1 1 0 00.877.876l.116.007h4.457l.001 4.454.007.116a1 1 0 00.876.877l.117.007.117-.007a1 1 0 00.876-.877l.007-.116V13h4.452l.116-.007a1 1 0 00.877-.876l.007-.117-.007-.117a1 1 0 00-.877-.876L17.455 11h-4.453l.001-4.455-.007-.117a1 1 0 00-.876-.877zM8.552.999h6.896c2.754 0 4.285.579 5.664 1.912 1.255 1.297 1.838 2.758 1.885 5.302L23 8.55v6.898c0 2.755-.578 4.286-1.912 5.664-1.298 1.255-2.759 1.838-5.302 1.885l-.338.003H8.552c-2.754 0-4.285-.579-5.664-1.912-1.255-1.297-1.839-2.758-1.885-5.302L1 15.45V8.551c0-2.754.579-4.286 1.912-5.664C4.21 1.633 5.67 1.05 8.214 1.002L8.552 1z"></path></svg>
     </router-link>
     <router-link to="/explore" class="mr-3">
-      <explore-svg></explore-svg>
+      <explore-svg  v-if="exploreNav"></explore-svg>
+      <svg v-if="!exploreNav" aria-label="Find People" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24"><path d="M13.173 13.164l1.491-3.829-3.83 1.49zM12.001.5a11.5 11.5 0 1011.5 11.5A11.513 11.513 0 0012.001.5zm5.35 7.443l-2.478 6.369a1 1 0 01-.57.569l-6.36 2.47a1 1 0 01-1.294-1.294l2.48-6.369a1 1 0 01.57-.569l6.359-2.47a1 1 0 011.294 1.294z"></path></svg>
     </router-link>
     <router-link to="/accounts/activity" class="mr-3">
-      <activity-svg></activity-svg>
+      <activity-svg v-if="activityNav"></activity-svg>
+      <svg v-if="!activityNav" aria-label="Activity Feed" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 48 48" width="24"><path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>
     </router-link>
     <li class="mr-3">
       <!-- Icons User Profile -->
