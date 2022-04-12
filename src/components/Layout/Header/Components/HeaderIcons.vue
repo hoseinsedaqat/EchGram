06d49be9dd8c010f1f1a6e22f9e355e4d79293cc
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-04-12 21:18:50 +0430

    add home svg dark cmp

--- src/components/Layout/Header/Components/HeaderIcons.vue
+++ src/components/Layout/Header/Components/HeaderIcons.vue
@@ -10,7 +10,8 @@ import SaveSvg from "./SaveSvg.vue";
 import SettingSvg from "./SettingSvg.vue";
 import SwitchSvg from "./SwitchSvg.vue";
 import DropdownAngle from "./DropdownAngle.vue";
-import {headerDarker} from '../Utils/HeaderDark'
+import HomeSvgDark from './HomeSvgDark.vue'
+import { headerDarker } from '../Utils/HeaderDark'
 
 export default Vue.extend({
   name: "HeaderIcons",
@@ -25,11 +26,15 @@ export default Vue.extend({
     SettingSvg,
     SwitchSvg,
     DropdownAngle,
+    HomeSvgDark,
   },
   mixins:[headerDarker],
   // now i think can change color of
   // watch: {
     
+  // },
+  // mounted() {
+  //   this.homeNav = false;
   // },
 });
 </script>
@@ -39,21 +44,7 @@ export default Vue.extend({
     <!-- Main Icons -->
     <router-link to="/" class="mr-3">
       <home-svg v-if="homeNav"></home-svg>
-      <svg
-        v-if="!homeNav"
-        aria-label="Home"
-        class="_8-yf5"
-        color="#262626"
-        fill="#262626"
-        height="24"
-        role="img"
-        viewBox="0 0 24 24"
-        width="24"
-      >
-        <path
-          d="M22 23h-6.001a1 1 0 01-1-1v-5.455a2.997 2.997 0 10-5.993 0V22a1 1 0 01-1 1H2a1 1 0 01-1-1V11.543a1.002 1.002 0 01.31-.724l10-9.543a1.001 1.001 0 011.38 0l10 9.543a1.002 1.002 0 01.31.724V22a1 1 0 01-1 1z"
-        ></path>
-      </svg>
+      <home-svg-dark v-if="!homeNav"></home-svg-dark>
     </router-link>
     <router-link to="/direct/inbox" class="mr-3">
       <direct-svg v-if="directNav"></direct-svg>
