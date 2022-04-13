80738ee99cbbd6ed58f100c71ceb57e9ce4dc844
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-04-14 00:58:29 +0430

    prepared activity pages to start

---
+++ src/views/Activity/ActivityView.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "ActivityView",
+});
+</script>
+
+
+
+<template>
+  <section id="activity">
+      
+  </section>
+</template>
