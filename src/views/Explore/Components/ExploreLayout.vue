6c7808c4bf15663761fb0a157b54b0b267e39d8a
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-07-21 20:05:20 +0430

    add/edit-some-stuff

--- src/views/Explore/Components/ExploreLayout.vue
+++ src/views/Explore/Components/ExploreLayout.vue
@@ -10,22 +10,22 @@ export default Vue.extend({
   <div class="container">
          <div class="row my-4">
              <div class="col-md-4">
-                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="img-fluid w-100 ex_ih">
-                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="mt-2 img-fluid w-100 ex_ih">
+                 <img src="@/assets/Images/Header/HotMe.jpg" alt="HoseinExplore" class="img-fluid w-100 ex_ih">
+                 <img src="@/assets/Images/Header/HotMe.jpg" alt="HoseinExplore" class="mt-2 img-fluid w-100 ex_ih">
              </div>
              <div class="col-md-8">
-                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="w-100 img-fluid">
+                 <img src="@/assets/Images/Header/HotMe.jpg" alt="HoseinExplore" class="w-100 img-fluid">
              </div>
          </div>
          <div class="row my-4">
              <div class="col-md-4">
-                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class="img-fluid w-100 mt-2">
+                 <img src="@/assets/Images/Header/HotMe.jpg" alt="HoseinExplore" class="img-fluid w-100 mt-2">
              </div>
              <div class="col-md-4">
-                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class=" img-fluid w-100 mt-2">
+                 <img src="@/assets/Images/Header/HotMe.jpg" alt="HoseinExplore" class=" img-fluid w-100 mt-2">
              </div>
              <div class="col-md-4">
-                 <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="HoseinExplore" class=" img-fluid w-100 mt-2">
+                 <img src="@/assets/Images/Header/HotMe.jpg" alt="HoseinExplore" class=" img-fluid w-100 mt-2">
              </div>
          </div>
     </div>
