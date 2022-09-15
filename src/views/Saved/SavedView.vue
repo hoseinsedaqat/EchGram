3f0b107a8248c23d13f9651a02d7b28f5aed2ffd
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-09-15 21:40:49 +0430

    add/dummy-saved-page-for-now

--- src/views/Saved/SavedView.vue
+++ src/views/Saved/SavedView.vue
@@ -1,13 +1,34 @@
-<template>
-  <section id="saved">
-    this is Saved Page
-  </section>
-</template>
-
-<script>
-export default {
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
   name: "SavedView",
-};
+});
 </script>
 
-<style scoped></style>
+<template>
+  <section id="saved">
+    <div class="container">
+      <h3 class="text-center mt-5 mb-3">Saved Posts</h3>
+      <div class="row">
+        <div class="col-md-4 my-2">
+          <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="PostOne" class="img-fluid">
+        </div>
+        <div class="col-md-4 my-2">
+          <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="PostTwo" class="img-fluid">
+        </div>
+        <div class="col-md-4 my-2">
+          <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="PostThree" class="img-fluid">
+        </div>
+        <div class="col-md-4 my-2">
+          <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="PostFour" class="img-fluid">
+        </div>
+        <div class="col-md-4 my-2">
+          <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="PostFive" class="img-fluid">
+        </div>
+        <div class="col-md-4 my-2">
+          <img src="@/assets/Images/Header/HoseinSedaqat.jpg" alt="PostSix" class="img-fluid">
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
