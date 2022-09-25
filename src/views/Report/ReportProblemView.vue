553b2443552eabb5f23096fa4a34029f93946d7a
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-09-25 21:23:25 +0330

    add/report-problem-text-area-data

--- src/views/Report/ReportProblemView.vue
+++ src/views/Report/ReportProblemView.vue
@@ -1,6 +1,22 @@
 <template>
   <section id="report-a-problem">
-    this is test
+    <div class="container">
+      <div class="row mt-5">
+        <div class="col-md-6 mx-auto">
+          <div class="card p-3">
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              class="form-control"
+              placeholder="please briefly write the problem"
+            ></textarea>
+            <button class="btn btn-primary w-100 my-2">Send</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -8,5 +24,16 @@
 import Vue from "vue";
 export default Vue.extend({
   name: "ReportProblemView",
+  data() {
+    return {
+      problemData: "",
+    };
+  },
 });
 </script>
+
+<style scoped>
+textarea {
+  resize: none;
+}
+</style>
