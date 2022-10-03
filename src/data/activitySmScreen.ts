cae1861cb0d3200d8ccf8c1e855f6fdcf934e70c
hoseinsedaqat <hoseinsedaqat6@gmail.com>
2022-10-03 17:06:38 +0330

    add/acivity-data-for-sm-size

---
+++ src/data/activitySmScreen.ts
@@ -0,0 +1,68 @@
+import Hosein from '@/assets/Images/User/Hosein_instagram_Picture.jpg';
+
+import Avicii from '@/assets/Images/User/Avicii_instagram_Picture.jpg';
+
+import Nima from '@/assets/Images/User/Nima_instagram_Picture.jpg';
+
+import Zoheir from '@/assets/Images/User/Zoheir_instagram_Picture.jpg';
+
+import Evan from '@/assets/Images/User/Evan_instagram_Picture.jpg';
+
+import MohammadReza from '@/assets/Images/User/MohammadReza_instagram_Picture.jpg';
+
+import Jadi from '@/assets/Images/User/Jadi_instagram_Picture.jpg';
+
+import Pouya from '@/assets/Images/User/Pouya_instagram_Picture.jpg';
+
+import Farzad from '@/assets/Images/User/Farzad_instagram_Picture.jpg';
+
+const activityLgScreen:{name:string, img:string, numberOfWeek: number}[]=[
+    {
+        name: 'Zoheir',
+        img: Zoheir,
+        numberOfWeek: 1
+    },
+    {
+        name: 'Hosein',
+        img: Hosein,
+        numberOfWeek: 2
+    },
+    {
+        name: 'Avicii',
+        img: Avicii,
+        numberOfWeek: 3
+    },
+    {
+        name: 'Nima',
+        img: Nima,
+        numberOfWeek: 2
+    },
+    {
+        name: 'Evan',
+        img: Evan,
+        numberOfWeek: 1
+    },
+    {
+        name: 'MohammadReza',
+        img: MohammadReza,
+        numberOfWeek: 2
+    },
+    {
+        name: 'Jadi',
+        img: Jadi,
+        numberOfWeek: 1
+    },
+    {
+        name: 'Pouya',
+        img: Pouya,
+        numberOfWeek: 2
+    },
+    {
+        name: 'Farzad',
+        img: Farzad,
+        numberOfWeek: 1
+    },
+]
+
+
+export default activityLgScreen;
--- src/views/Activity/ActivityView.vue
+++ src/views/Activity/ActivityView.vue
@@ -1,8 +1,15 @@
 <script lang="ts">
 import Vue from "vue";
 
+import activitySmScreen from '@/data/activitySmScreen'
+
 export default Vue.extend({
   name: "ActivityView",
+  data(){
+    return {
+      activitySmScreen
+    }
+  }
 });
 </script>
 
@@ -29,21 +36,22 @@ export default Vue.extend({
             <i class="fas fa-angle-right text-primary"></i>
           </div>
           <!-- Activity -->
-          <div class="d-flex align-items-center justify-content-between my-4" v-for="x in 10" :key="x">
+          <div class="d-flex align-items-center justify-content-between my-4" v-for="(user,idx) in activitySmScreen" :key="(user,idx)">
             <div class="d-flex align-items-center" style="font-size: 12px">
               <img
-                src="@/assets/Images/Header/HoseinSedaqat.jpg"
+                :src="user.img"
                 class="mr-2"
                 alt=""
                 width="40px"
                 style="border-radius: 50%"
               />
               <span class="d-flex flex-row">
-                <span class="text-dark font-weight-bold mr-1">HoseinSedaqat</span>
+                <span class="text-dark font-weight-bold mr-1">{{ user.name }}</span>
                 <span class="mr-1">Liked Your Photo</span>
-                <span class="text-muted">2w</span>
+                <span class="text-muted">{{ user.numberOfWeek }}w</span>
               </span>
             </div>
+            <!-- Change each photo -->
             <img
                 src="@/assets/Images/Header/HoseinSedaqat.jpg"
                 class="mr-2"
